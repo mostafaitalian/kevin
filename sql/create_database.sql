25fba515c7a4f622155522e349d0e06d7852aa2a
mostafaitalian <eng_mustafa_yossef@hotmail.com>
2019-03-04 00:15:20 +0200

    my first commit

---
+++ sql/create_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database.
+
+Note: ONLY development database;
+*/
+
+CREATE USER styleproject SUPERUSER;
+CREATE DATABASE styleproject OWNER styleproject ENCODING 'utf-8';
